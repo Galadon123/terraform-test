329a7cc70387d48b691122f13a7e04eae1d36593
Fazlul Karim <fazlulkarim362@gmail.com>
2024-08-15 18:19:57 +0600

    update-1

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
+terraform {
+  backend "remote" {
+    organization = "terraform_projects_poridhi"
+
+    workspaces {
+      name = "poridhi-terraform"
+    }
+  }
+}
+
 variable "ssh_public_key" {
-  type = string
+  type        = string
   description = "The public SSH key to be used for the EC2 instance"
 }
 
@@ -38,6 +48,13 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -66,4 +83,44 @@ resource "aws_instance" "ec2" {
   }
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update and install necessary packages
+              sudo apt-get update
+              sudo apt-get upgrade -y
+
+              # Install Docker
+              sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo apt-get update
+              sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+              # Install the latest Node.js version
+              curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+              sudo apt-get install -y nodejs
+
+              # Install VS Code Server
+              curl -fsSL https://code-server.dev/install.sh | sh
+
+              # Create VS Code Server systemd service
+              echo '[Unit]
+              Description=VS Code Server
+              After=network.target
+
+              [Service]
+              Type=simple
+              User=ubuntu
+              Environment="PASSWORD=105925"
+              ExecStart=/usr/bin/code-server --bind-addr 0.0.0.0:8080 /home/ubuntu
+              Restart=on-failure
+
+              [Install]
+              WantedBy=multi-user.target' | sudo tee /etc/systemd/system/code-server.service
+              
+              sudo systemctl daemon-reload
+              sudo systemctl enable code-server
+              sudo systemctl start code-server
+            EOF
 }

fe2814edbfd10d9c19bd590c4564de1d5a6f950c
Fazlul Karim <fazlulkarim362@gmail.com>
2024-08-16 19:28:38 +0600

    create-ec2

--- main.tf
+++ main.tf
@@ -8,27 +8,34 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region = "ap-southeast-1"
 }
 
-# Fetch the default VPC
-data "aws_vpc" "default" {
-  default = true
-}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.0"
 
-# Fetch all subnets in the default VPC
-data "aws_subnets" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-# Select the first subnet based on the index
-data "aws_subnet" "default" {
-  id = data.aws_subnets.default.ids[0]
+  azs            = ["ap-southeast-1a"]
+  public_subnets = ["10.0.1.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_security_group" "ec2_sg" {
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     from_port   = 22
@@ -64,9 +71,8 @@ resource "aws_key_pair" "main" {
 resource "aws_instance" "ec2" {
   ami           = "ami-060e277c0d4cce553"  # Example Ubuntu AMI
   instance_type = "t2.micro"
-  subnet_id     = data.aws_subnet.default.id
+  subnet_id     = module.vpc.public_subnets[0]
   key_name      = aws_key_pair.main.key_name
-
   user_data = <<-EOF
               #!/bin/bash
               # Update and install necessary packages
@@ -95,7 +101,7 @@ resource "aws_instance" "ec2" {
               [Service]
               Type=simple
               User=ubuntu
-              Environment="PASSWORD=105925"
+              Environment="PASSWORD=2525"
               ExecStart=/usr/bin/code-server --bind-addr 0.0.0.0:8080
               Restart=on-failure
 
@@ -106,10 +112,10 @@ resource "aws_instance" "ec2" {
               sudo systemctl daemon-reload
               sudo systemctl enable --now code-server
               EOF
-
   tags = {
-    Name = "public-ec2-instance"
+    Name = "server"
   }
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
 }

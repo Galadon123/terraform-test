36b3bf421ab98684b692e2abecfb94f2bb120d04
Fazlul Karim <fazlulkarim362@gmail.com>
2024-08-16 19:56:22 +0600

    create-ec2

--- main.tf
+++ main.tf
@@ -1,73 +1,3 @@
-terraform {
-  backend "remote" {
-    organization = "terraform_projects_poridhi"
-
-    workspaces {
-      name = "poridhi-terraform"
-    }
-  }
-}
-
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.0"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs            = ["us-east-1a"]
-  public_subnets = ["10.0.1.0/24"]
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  map_public_ip_on_launch = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
-resource "aws_security_group" "ec2_sg" {
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ec2-sg"
-  }
-}
-
-resource "aws_key_pair" "main" {
-  key_name   = "id_rsa-key-1"
-  public_key = var.ssh_public_key
-}
-
 resource "aws_instance" "ec2" {
   ami           = "ami-04a81a99f5ec58529"  # Example Ubuntu AMI
   instance_type = "t2.micro"
@@ -108,7 +38,7 @@ resource "aws_instance" "ec2" {
               [Install]
               WantedBy=multi-user.target' | sudo tee /etc/systemd/system/code-server.service
 
-              # Reload systemd to apply changes
+              # Reload systemd to apply changes and start VS Code Server
               sudo systemctl daemon-reload
               sudo systemctl enable --now code-server
               EOF
@@ -117,5 +47,4 @@ resource "aws_instance" "ec2" {
   }
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-
 }
